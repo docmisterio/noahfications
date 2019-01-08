ff5e7aaab9787f00b780e09603d054692bf75c56
Zach Becker <docmisterio@Trevor-the-MacBook-Pro.local>
2019-01-07 20:42:22 -0600

    finished the thing but it's not working, something about not getting icon back from the API

--- Noahfications/ViewController.swift
+++ Noahfications/ViewController.swift
@@ -1,9 +1,13 @@
+import Foundation
 import UIKit
+import Alamofire
+import SwiftyJSON
+import NVActivityIndicatorView
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var locationLabel: UILabel!
-    
     @IBOutlet weak var dayLabel: UILabel!
     @IBOutlet weak var conditionImage: UIImageView!
     @IBOutlet weak var conditionLabel: UILabel!
@@ -12,13 +16,72 @@ class ViewController: UIViewController {
     
     let gradientLayer = CAGradientLayer()
     
+    let apiKey = "f8850e9145a838c23ef5edf09bc7ac13"
+    var lat = 11.344533
+    var lon = 104.33322
+    var activityIndicator: NVActivityIndicatorView!
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         backGroundView.layer.addSublayer(gradientLayer)
+        
+        let indicatorSize: CGFloat = 70
+        let indicatorFrame = CGRect(x: (view.frame.width-indicatorSize) / 2, y: (view.frame.height-indicatorSize) / 2, width: indicatorSize, height: indicatorSize)
+        activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color: UIColor.white, padding: 20.0)
+        activityIndicator.backgroundColor = UIColor.black
+        view.addSubview(activityIndicator)
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        activityIndicator.startAnimating()
+        if (CLLocationManager.locationServicesEnabled()) {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
         }
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         setBlueBackGradientBackGround()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        lat = location.coordinate.latitude
+        lon = location.coordinate.longitude
+        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&units=imperial").responseJSON {
+            response in
+            self.activityIndicator.stopAnimating()
+            if let responseString = response.result.value {
+                let jsonResponse = JSON(responseString)
+                let jsonWeather = jsonResponse["weather"].array![0]
+                let jsonTemp = jsonResponse["main"]
+                let iconName = jsonResponse["icon"].stringValue
+                
+                self.locationLabel.text = jsonResponse["name"].stringValue
+                self.conditionImage.image = UIImage(named: iconName)
+                self.conditionLabel.text = jsonWeather["main"].stringValue
+                self.tempLabel.text = "\(Int(round(jsonTemp["temp"].doubleValue)))"
+                
+                let date = Date()
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "EEEE"
+                self.dayLabel.text = dateFormatter.string(from: date)
+                
+                let dayCycleSuffix = iconName.suffix(1)
+                if (dayCycleSuffix == "n") {
+                    self.setGreyGradientBackGround()
+                } else {
+                    self.setBlueBackGradientBackGround()
+                }
+            }
+        }
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
     }
     
     override func didReceiveMemoryWarning() {
